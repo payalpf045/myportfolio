85ab850f1b6d5e2a2a172966cde79cccb5287e88
Ankit Kumar <kdmspokharahan@gmail.com>
2025-09-30 09:07:32 +0000

    what the fuck is this i said i want my old working site back 9842d9a

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
 export function createSupabaseClient() {
-  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+  const supabaseUrl = "https://ydhesvqbtgxqzbsellxy.supabase.co"
+  const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlkaGVzdnFidGd4cXpic2VsbHh5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTg2MjM2ODUsImV4cCI6MjAzNDE5OTY4NX0.n2N7Jq_B6S_y_i4sH_M4z1sT5-7f_x2nLq_M-2f1vYQ"
 
   if (!supabaseUrl || !supabaseAnonKey) {
     throw new Error('Supabase URL or Anon Key is not defined in environment variables.');
